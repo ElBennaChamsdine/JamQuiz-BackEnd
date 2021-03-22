7121544831aafbfb7b284d7497d630d8a8d2a08c
roflan94 <amir.bennasr@esprit.tn>
2021-03-23 00:01:24 +0100

    oklm

--- public/index.php
+++ public/index.php
@@ -9,13 +9,13 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_ENV']) {
     umask(0000);
 
     Debug::enable();
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_ENV']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
